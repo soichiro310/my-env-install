a7e1946a7a723f6533a313f49edc48a1cb098b20
soichiro310 <so16.310@gmail.com>
2020-11-19 17:44:21 +0900

    手順直した

--- README.md
+++ README.md
@@ -5,9 +5,10 @@
 
 ## setup order
 
-1. git clone https://github.com/h173309/my-env-install.git
+1. git clone https://github.com/soichiro310/my-env-install.git
 1. cd my-env-install
+1. git checkout -b feature/command_fix origin/feature/command_fix
 1. sh setup.sh
 1. conda create -n general
 1. sh general.sh
-1. echo 'conda activate general' >> ~/.bashrc
+1. echo "conda activate general" >> ~/.bashrc
--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@
 ## 20191014
 
 cd ~
-
+CURRENT=`pwd`
+# IP=`ifconfig | grep -A 1 'em1' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1`
+SET_PORT=9876
 ##########################################################################
 ## install Anaconda 3 ####################################################
 ##########################################################################
@@ -19,18 +21,22 @@ sh Anaconda3-2019.07-Linux-x86_64.sh
 jupyter notebook --generate-config
 
 # SHA_TMP < sha_hash
+echo ""
 SHA_TMP=`ipython -c 'from IPython.lib import passwd; passwd()' | cut -d' ' -f3`
 # allow all ip
-echo -e "c.NotebookApp.ip = '0.0.0.0'"                                                                 >> ~/.jupyter/jupyter_notebook_config.py
-echo -e "c.NotebookApp.password = u${SHA_TMP}"                                                         >> ~/.jupyter/jupyter_notebook_config.py
-echo -e "c.NotebookApp.open_browser = False"                                                           >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.ip = '133.15.24.19'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.password = u${SHA_TMP}" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
 # port number is able to change if you want
-echo -e "c.NotebookApp.port = 8888"                                                                    >> ~/.jupyter/jupyter_notebook_config.py
-# need to 'environment_kernels'
-echo -e "c.NotebookApp.kernel_spec_manager_class = 'environment_kernels.EnvironmentKernelSpecManager'" >> ~/.jupyter/jupyter_notebook_config.py
-echo -e "c.EnvironmentKernelSpecManager.conda_env_dirs=['${HOME}/anaconda3/envs/']"                    >> ~/.jupyter/jupyter_notebook_config.py
-unset SHA_TMP
+echo "c.NotebookApp.port = '${SET_PORT}'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.notebook_dir = '${CURRENT}'" >> ~/.jupyter/jupyter_notebook_config.py
 
+# need to 'environment_kernels'
+# echo -e "c.NotebookApp.kernel_spec_manager_class = 'environment_kernels.EnvironmentKernelSpecManager'" >> ~/.jupyter/jupyter_notebook_config.py
+# echo -e "c.EnvironmentKernelSpecManager.conda_env_dirs=['${HOME}/anaconda3/envs/']"                    >> ~/.jupyter/jupyter_notebook_config.py
+unset SHA_TMP
+unset CURRENT
+unset SET_PORT
 # enable to change conda-env in jupyter
 pip install environment_kernels
 # enable Tensorboard in jupyter
